7795ff45092424bbcd647c18f816ef0d8a238851
xiaohongyan <xiaohongyan2015@gmail.com>
2026-04-27 12:35:05 -0500

    Add: kth-largest-element-in-an-array - submission-10

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-10.py
@@ -0,0 +1,38 @@
+import random
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+
+        def partition(nums, left, right):
+            # 随机取pivot
+            pivot_idx = random.randint(left, right)
+            nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+            pivot = nums[right]
+
+            i = left
+            for j in range(left, right):
+                if nums[j] < pivot:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+                
+            nums[i], nums[right] = nums[right], nums[i]
+            
+            return i
+
+        target_idx = len(nums) - k
+        left = 0
+        right =len(nums) - 1
+        while True:
+            idx = partition(nums, left, right)
+            if idx == target_idx:
+                return nums[target_idx]
+            if idx < target_idx:
+                left = idx + 1
+            else:
+                right = idx - 1
+
+        return nums[target_idx]
+                
+            
+
