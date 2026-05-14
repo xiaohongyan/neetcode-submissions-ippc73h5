c7cbd12b3a477fea7a087ef5ac832f0b9a581c3a
xiaohongyan <xiaohongyan2015@gmail.com>
2026-05-13 21:09:44 -0500

    Add: surrounded-regions - submission-4

---
+++ Data Structures & Algorithms/surrounded-regions/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        toOcean = set()
+        directions = [[1, 0], [-1, 0], [0, -1], [0, 1]]
+
+        def dfs(r, c, toOcean):
+
+            if r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in toOcean or board[r][c]  !='O':
+                return
+
+            toOcean.add((r, c))
+            for d in directions:
+                nr = r + d[0]
+                nc = c + d[1]
+                dfs(nr, nc, toOcean)
+
+
+        for r in range(ROWS):
+            dfs(r, 0, toOcean)
+            dfs(r, COLS - 1,toOcean)
+
+        for c in range(COLS):
+            dfs(0, c, toOcean)
+            dfs(ROWS - 1, c, toOcean)
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c]  =='O' and (r, c) not in toOcean:
+                    board[r][c]  ='X'
