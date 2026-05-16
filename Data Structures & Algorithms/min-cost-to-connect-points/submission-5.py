661b0bfbebffc3cbe9642a013dc256fa0dda761a
xiaohongyan <xiaohongyan2015@gmail.com>
2026-05-15 22:27:20 -0500

    Add: min-cost-to-connect-points - submission-5

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-5.py
@@ -0,0 +1,60 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, i):
+        root = i
+        while root != self.parent[root]:
+            root =  self.parent[root]
+
+        while self.parent[i] != root:
+            tmp = self.parent[i]
+            self.parent[i] = root
+            i = tmp
+
+        return root
+
+    def union(self, i, j):
+        root_i = self.find(i)
+        root_j = self.find(j)
+
+        if root_i == root_j:
+            return False
+
+        if self.size[root_i] > self.size[root_j]:
+            root_i, root_j = root_j, root_i
+
+        self.size[root_j] += self.size[root_i]
+        self.parent[root_i] = root_j
+
+        return True
+
+class Solution:
+    # Kruskal
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        n = len(points)
+        edges = []
+        minHeap = []
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                edges.append((dist, i, j))
+
+        heapq.heapify(edges)
+
+        edge_cnt = 0
+        dsu = DSU(n)
+        res = 0
+        while edge_cnt < n - 1 and edges:
+            dist, i, j = heapq.heappop(edges)
+            if dsu.union(i, j):
+                 edge_cnt += 1
+                 res += dist
+        
+        return res
+            
