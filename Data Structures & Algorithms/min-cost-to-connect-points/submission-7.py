810aea36f3c2c6532f5692b781624c6372888b3f
xiaohongyan <xiaohongyan2015@gmail.com>
2026-05-15 22:34:22 -0500

    Add: min-cost-to-connect-points - submission-7

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-7.py
@@ -0,0 +1,50 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, i):
+        if i != self.parent[i]:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i, j):
+        root_i = self.find(i)
+        root_j = self.find(j)
+
+        if root_i == root_j:
+            return False
+
+        if self.size[root_i] > self.size[root_j]:
+            root_i, root_j = root_j, root_i
+
+        self.size[root_j] += self.size[root_i]
+        self.parent[root_i] = root_j
+
+        return True
+
+class Solution:
+    # Kruskal
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        n = len(points)
+        edges = []
+        minHeap = []
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                edges.append((dist, i, j))
+
+        edges.sort()
+        dsu = DSU(n)
+        res = 0
+
+        for dist, i, j in edges:
+            if dsu.union(i, j):
+                res += dist
+        
+        return res
+            
