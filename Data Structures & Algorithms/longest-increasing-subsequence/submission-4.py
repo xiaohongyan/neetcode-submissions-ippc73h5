0e2fb7eefc70114f3468a0922e1aea90ec27bb95
xiaohongyan <xiaohongyan2015@gmail.com>
2026-05-21 11:13:10 -0500

    Add: longest-increasing-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        memo = [0] * n
+        res = 0
+  
+        def dfs(i):  # the length of the longest increasing subsequence starting at index i
+            if i >= n:
+                return 0
+            if memo[i] != 0:
+                return memo[i]
+
+            cur = 1
+            for j in range(i + 1, n):
+                tmp = 1
+                if nums[j] > nums[i]:
+                    tmp += dfs(j)
+                    cur = max(cur, tmp)
+
+            memo[i] = cur
+            return cur
+
+        return max(dfs(i) for i in range(n))
+
