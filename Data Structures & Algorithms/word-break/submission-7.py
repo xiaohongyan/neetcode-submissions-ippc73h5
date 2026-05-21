4daf8d4e1c871e9d662ea78eadb019a66b29429b
xiaohongyan <xiaohongyan2015@gmail.com>
2026-05-21 15:07:04 -0500

    Add: word-break - submission-7

---
+++ Data Structures & Algorithms/word-break/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        if not wordDict:
+            return False
+
+        n = len(s)
+        dp = [False] * (n + 1)  # dp[i] means whether the substring s[i:] can be segmented
+        dp[n] = True
+
+        for i in range(n -1, -1, -1):
+            for w in wordDict:
+                if i + len(w) - 1 < n and  s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                
+                if dp[i]:
+                    break
+
+        return dp[0]
